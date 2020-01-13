bf83fe3cd2ab019d5c784ba23d7e5d10665e5783
mrbuhamad <mr.buhamad@hotmail.com>
2020-01-13 19:21:25 +0300

    add task 4

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    opening_time = models.TimeField(auto_now_add=True)
+    closing_time = models.TimeField(max_length=120)
